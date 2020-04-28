5e2e69c5980a99a6e629f0d8e2f4789e0062c06d
thetabyte <seunlatunde@gmail.com>
2020-04-28 19:11:51 +0100

    first js commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let studentname = "Seun Latunde";
+console.log("My name is: " + studentname);
+
+let courses  = ["html","css","js"];
+console.log(courses);
+
+const data = [];
+
+for (let i=0; i<200; i++) {
+
+  if (i % 2 !==0) {
+
+    data.push(i);
+
+  }
+
+}
+
+console.log(data);
